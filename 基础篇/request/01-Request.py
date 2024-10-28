78345e57d9c5f66b0246caca65ca43b272e3a7d3
lwj <jg15575446613@163.com>
2024-10-29 00:32:43 +0800

    feat: os.path.dirname(__file__)获取当前文件执行的目录

--- 基础篇/request/01-Request.py
+++ 基础篇/request/01-Request.py
@@ -1,9 +1,14 @@
 import requests
+import os
 
 if __name__ == "__main__":
+  file_name = 'sogou.html'
+  dir_name = os.path.dirname(__file__)
+  file_path = os.path.join(dir_name, file_name)
+  print(file_path)
   url = 'https://www.baidu.com/'
   response = requests.get(url=url)
   page_txt = response.text
-  with open('./sogou.html', 'w', encoding='utf-8') as fp:
+  with open(file_path, 'w', encoding='utf-8') as fp:
     fp.write(page_txt)
   print('爬取数据结束!')
